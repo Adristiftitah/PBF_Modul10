8ae0b97faabba34cb91ba229927e00a59385599a
Adristiftitah <adristiiftitahyuniar@gmail.com>
2021-04-14 14:04:38 +0700

    praktikum dan tugas

---
+++ src/firebase.config.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+export default {
+    apiKey: "AIzaSyDSaUpwEMJwE7h7U83LmriW5hT3hMo5ysM",
+    authDomain: "react-firebase-adristi.firebaseapp.com",
+    projectId: "react-firebase-adristi",
+    storageBucket: "react-firebase-adristi.appspot.com",
+    messagingSenderId: "647063552367",
+    appId: "1:647063552367:web:4436b1190db758da87a89a",
+    measurementId: "G-33M49D9X0Y"
+  };
